292ec2e9313f7511a40de4b3d1861b3843cb5d46
Blaž <22869613+xBlaz3kx@users.noreply.github.com>
2021-11-09 21:47:38 +0100

    Preparing models

--- model/group/group.go
+++ model/group/group.go
@@ -1,1 +1,16 @@
 package group
+
+type (
+	Group struct {
+		Name  string   `json:"name" bson:"name" validate:"required"`
+		Users []string `json:"users" bson:"users"`
+	}
+)
+
+//NewGroup creates a new group object
+func NewGroup(name string) (*Group, error) {
+	return &Group{
+		Name:  name,
+		Users: []string{},
+	}, nil
+}
--- model/user/user.go
+++ model/user/user.go
@@ -1,1 +1,18 @@
 package user
+
+type (
+	User struct {
+		Email    string `json:"email" bson:"email" validate:"required,email"`
+		Password string `json:"password" bson:"password" validate:"required"`
+		Name     string `json:"name"  bson:"name" validate:"required"`
+	}
+)
+
+//NewUser creates a new user object
+func NewUser(email, name, password string) (*User, error) {
+	return &User{
+		Email:    email,
+		Password: name,
+		Name:     password,
+	}, nil
+}
